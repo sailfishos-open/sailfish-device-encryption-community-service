6dec64e6240929c9eef7d2deb1b4c530366e01a6
Rinigus <rinigus.git@gmail.com>
2021-09-11 12:34:21 +0300

    add support for adding passwords

--- src/service.cpp
+++ src/service.cpp
@@ -1,7 +1,10 @@
 #include "service.h"
 
 #include <QDBusConnection>
+#include <QSharedPointer>
+
 #include <devenc/devicelist.h>
+#include <devenc/passwordmaker.h>
 
 //#warning "SWAP THE BUS"
 //#define BUS QDBusConnection::sessionBus()
@@ -11,7 +14,7 @@
 #define DEVCHECK(d, ret) \
   if (d == nullptr) \
     { \
-      QDBusMessage error = message.createErrorReply(QDBusError::InvalidArgs, "Could not find device"); \
+      QDBusMessage error = message.createErrorReply(QDBusError::InvalidArgs, tr("Could not find device")); \
       BUS.send(error); \
       return ret; \
     } \
@@ -19,11 +22,23 @@
 
 Service::Service(QObject *parent) : QObject(parent)
 {
+  // it is expected that description is unique for each password type
+  for (QString i: DevEnc::PasswordMaker::instance()->types())
+    m_password_types[DevEnc::PasswordMaker::instance()->description(i)] = i;
 }
 
 QStringList Service::Devices()
 {
   return DevEnc::DeviceList::instance()->devices();
+}
+
+QStringList Service::PasswordTypes()
+{
+  QStringList r;
+  // keep the same order as a matter of preference
+  for (QString i: DevEnc::PasswordMaker::instance()->types())
+    r.append(DevEnc::PasswordMaker::instance()->description(i));
+  return r;
 }
 
 QString Service::DeviceName(QString id, const QDBusMessage &message)
@@ -66,4 +81,41 @@ int Service::UsedPasswordSlots(QString id, const QDBusMessage &message)
   DevEnc::Device *d = DevEnc::DeviceList::instance()->device(id);
   DEVCHECK(d, 0);
   return d->usedPasswordSlots();
+}
+
+DevEnc::Password *Service::makePassword(QString description)
+{
+  QString type = m_password_types.value(description, QString());
+  return DevEnc::PasswordMaker::instance()->newPassword(type);
+}
+
+bool Service::AddPassword(QString id,
+                          QString controlPassword, QString controlType,
+                          QString newPassword, QString newType,
+                          const QDBusMessage &message)
+{
+  DevEnc::Device *d = DevEnc::DeviceList::instance()->device(id);
+  DEVCHECK(d, false);
+
+  QSharedPointer<DevEnc::Password> controlPwd(makePassword(controlType));
+  QSharedPointer<DevEnc::Password> newPwd(makePassword(newType));
+
+  if (!controlPwd || !newPwd)
+    {
+      QDBusMessage error = message.createErrorReply(QDBusError::InvalidArgs, tr("Unknown password type"));
+      BUS.send(error);
+      return false;
+    }
+
+  controlPwd->setPassword(controlPassword);
+  newPwd->setPassword(newPassword);
+
+  if (!d->addPassword(controlPwd.data(), newPwd.data()))
+    {
+      QDBusMessage error = message.createErrorReply(QDBusError::InvalidArgs, tr("Failed to add new password. Check that the control password is correct."));
+      BUS.send(error);
+      return false;
+    }
+
+  return true;
 }
--- src/service.h
+++ src/service.h
@@ -2,8 +2,11 @@
 #define SERVICE_H
 
 #include <QDBusMessage>
+#include <QMap>
 #include <QObject>
 #include <QStringList>
+
+#include <devenc/password.h>
 
 class Service : public QObject
 {
@@ -15,8 +18,9 @@ public:
 
 public slots:
 
-  // list of devices
+  // general properties
   QStringList Devices();
+  QStringList PasswordTypes();
 
   // device parameters
   QString DeviceName(QString id, const QDBusMessage &message);
@@ -26,11 +30,23 @@ public slots:
   QString RecoveryPassword(QString id, const QDBusMessage &message);
   bool RemoveRecoveryPasswordCopy(QString id, const QDBusMessage &message);
 
+  // state of password slots
   int FreePasswordSlots(QString id, const QDBusMessage &message);
   int UsedPasswordSlots(QString id, const QDBusMessage &message);
 
+  // password manipulation
+  bool AddPassword(QString id,
+                   QString controlPassword, QString controlType,
+                   QString newPassword, QString newType,
+                   const QDBusMessage &message);
+
 signals:
 
+private:
+  DevEnc::Password *makePassword(QString description);
+
+private:
+  QMap<QString, QString> m_password_types;
 };
 
 #endif // SERVICE_H
